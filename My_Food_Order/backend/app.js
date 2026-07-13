3079087262bddc2701a4d123715672936f5c5210
Bhargav958 <pallebhargav0705@gmail.com>
2026-07-13 12:28:15 +0530

    modified the cors for deployment

--- My_Food_Order/backend/app.js
+++ My_Food_Order/backend/app.js
@@ -19,7 +19,12 @@ const payment = require("./routes/payment");
 const cart = require("./routes/cart");
 
 // Middlewares
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true
+}));
+
 app.use(cookieParser());
 app.use(fileUpload());
 
